3165ffe1281521b5be7949dc33232b43b09a8c42
somelamedude-git <cozydeveloper843@gmail.com>
2025-06-15 18:59:49 +0530

    Response handler added

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,10 @@
+class ApiResponse{
+    constructor(message='Success', statusCode, data){
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = data;
+        this.success = statusCode < 400;
+    }
+};
+
+module.exports = { ApiResponse };
