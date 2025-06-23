93d9985adf4d8937f50268a69aa60a7c8fb48581
somelamedude-git <cozydeveloper843@gmail.com>
2025-06-24 01:58:48 +0530

    cloudinary configured and ready to use

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -8,3 +8,21 @@ cloudinary.config({
     api_secret: process.env.CLOUD_SECRET
 });
 
+const uploadOnCloudinary = async(localFilePath)=>{
+    try{
+          if(!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+        resource_type: "auto"
+    });
+    fs.unlinkSync(localFilePath); //We have to keep our laptops from exploding
+    return response;
+    } catch(error){
+        fs.unlinkSync(localFilePath);
+        return null;
+    }
+}
+
+module.exports = {
+    uploadOnCloudinary
+}
+
