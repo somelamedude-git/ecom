9a9782cd55f5d2bb1065698072263763c2d663bd
somelamedude-git <cozydeveloper843@gmail.com>
2025-06-27 04:28:49 +0530

    Create user tested and perfect

--- src/utils/tokens.utils.js
+++ src/utils/tokens.utils.js
@@ -4,8 +4,8 @@ const { BaseUser } = require('../models/user.models')
 const generateAccessAndRefreshTokens = async(userId)=>{
     try{
         const user = await BaseUser.findById(userId);
-        const accessToken = user.generateAcessToken();
-        const refreshToken = user.generateRefreshAcessToken();
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshAccessToken();
 
         user.refreshToken = refreshToken;
         await user.save({validateBeforeSave: false});
