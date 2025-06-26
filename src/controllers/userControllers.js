478d660f4f26ffd0a3984e5a1aca04e7f71935ce
somelamedude-git <cozydeveloper843@gmail.com>
2025-06-26 21:40:43 +0530

    Added google auth in the controller file with cookies

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 const {verifyEmail} = require('../utils/verification.util');
 const { Response, Request, CookieOptions } = require('express');
 const { generateAccessAndRefreshTokens, generateAcessAndRefreshTokens } = require('../utils/tokens.utils');
+const axios = require('axios');
 require('dotenv').config({ path: '../.env' });
 
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
@@ -40,6 +41,48 @@ const createUser = asyncHandler(async (req, res) => {
                                         name: user.name,
                                         kind: user.kind
                                  }})
+});
+
+const googleLogin = asyncHandler(async(req, res)=>{
+    const { code } = req.query;
+   const { data } = await axios.post('https://oauth2.googleapis.com/token', {
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      client_secret: process.env.GOOGLE_CLIENT_SECRET,
+      code,
+      redirect_uri: process.env.GOOGLE_REDIRECT_URI,
+      grant_type: 'authorization_code',
+    });
+
+    const { access_token, id_token } = data;
+
+    const { data: profile } = await axios.get('https://www.googleapis.com/oauth2/v1/userinfo', {
+      headers: { Authorization: `Bearer ${access_token}` },
+    });
+
+    const user_email = profile.email;
+    
+    let user = await BaseUser.findOne({email: user_email});
+
+    if(user){
+        const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(user._id);
+        const loggedInUser = await BaseUser.findById(user._id).select("-password -refreshToken");
+
+        const options = {
+            httpOnly: true,
+            secure: true
+        }
+        
+        return res.status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(
+            {
+                success:true,
+                id: loggedInUser._id,
+                email: loggedInUser.email
+            }
+        )
+    }
 });
 
 const getUser = asyncHandler(async(req, res) => {
@@ -155,6 +198,7 @@ const verifyUser = asyncHandler(async (req, res) => {
 
 module.exports = {
     createUser,
+    googleLogin,
     getUser,
     updateUser,
     deleteUser,
