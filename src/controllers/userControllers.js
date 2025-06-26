9225583191689db41ed96e04f1ac3236deeb5fd3
somelamedude-git <cozydeveloper843@gmail.com>
2025-06-26 23:10:44 +0530

    cookies configured while creating account

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -3,41 +3,37 @@ const {OAuth2Client} = require('google-auth-library');
 const { asyncHandler } = require("../utils/asyncHandler");
 const jwt = require('jsonwebtoken');
 const {verifyEmail} = require('../utils/verification.util');
-const { generateAccessAndRefreshTokens, generateAcessAndRefreshTokens } = require('../utils/tokens.utils');
 const axios = require('axios');
+const { ApiError } = require('../utils/ApiError');
+const { generateAccessAndRefreshTokens } = require('../utils/tokens.utils');
 require('dotenv').config({ path: '../.env' });
 
 const createUser = asyncHandler(async (req, res) => {
 
-        const {kind, username, email, password, name, googleLogin, coverImage} = req.body;
+        const {kind, username, email, password, name} = req.body;
 
         const existingUser = await BaseUser.findOne({email: email.toLowerCase().trim()})
 
         if(existingUser)
-            return res.status(409).json({status: false, message: "Email already exists"})
+            throw new ApiError(409, "You are already registered");
         
         const userKinds = {Buyer, Seller, Admin};
         const UserKind = userKinds[kind];
 
         if(!UserKind)
-            return res.status(400).json({status: false, message: "User kind not found"})
+            throw new ApiError(400, "User kind not found")
 
+        const user = new UserKind({username:username, email:email, password:password, name:name});
+        await user.save();
 
-        const user = new UserKind({username, email, password, name, address, googleLogin, coverImage});
-        const refreshToken = user.generateRefreshAccessToken();
-        user.refreshToken = refreshToken;
+        const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(user._id);
 
-        await user.save()
+         res
+        .cookie('accessToken', accessToken, { httpOnly: true, secure: true })
+        .cookie('refreshToken', refreshToken, { httpOnly: true, secure: true })
+        .status(200)
+        .json({ success: true, userId: user._id, email: user.email, message:"User created successfully" });
 
-        return res.status(201).json({status: true, 
-                                    message: "User created successfully",
-                                    User: {
-                                        id: user._id,
-                                        username: user.username,
-                                        email: user.email,
-                                        name: user.name,
-                                        kind: user.kind
-                                 }})
 });
 
                            
