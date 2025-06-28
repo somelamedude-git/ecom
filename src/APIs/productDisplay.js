a7a5a1bf10d83c8bcfe8c8129baeafd2d52deab5
somelamedude-git <cozydeveloper843@gmail.com>
2025-06-28 19:32:01 +0530

    Dashboard, search, wishlist and cart to be dealt by fetchProducts

---
+++ src/APIs/productDisplay.js
@@ -0,0 +1,20 @@
+const { Product } = require('../models/product.models');
+const { asyncHandler } = require('../utils/asyncHandler');
+const { ApiError } = require('../utils/ApiError');
+
+const fetchProducts = asyncHandler(async (req, res)=>{
+    const limit = Number(req.query.limit) || 100;
+    const products = await Product.find().limit(limit). populate('category').populate('owner', 'name email');
+
+    if(products.length == 0){
+        throw new ApiError('404', "Products not found");
+    }
+
+    res.status(200).json({
+        products
+    });
+});
+
+module.exports = {
+    fetchProducts
+}
