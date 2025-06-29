b34669d94ea474c870a29f8c4ad99e7d9cc3bd08
somelamedude-git <cozydeveloper843@gmail.com>
2025-06-29 21:10:09 +0530

    single product fetch api addition

--- src/APIs/productDisplay.js
+++ src/APIs/productDisplay.js
@@ -3,6 +3,11 @@ const { asyncHandler } = require('../utils/asyncHandler');
 const { ApiError } = require('../utils/ApiError');
 
 const fetchProducts = asyncHandler(async (req, res)=>{
+    if(req.params.productId){
+        const product = await Product.findById(req.params.productId);
+        if(!product) throw new ApiError(404, 'The product you requested for does not exist');
+        return res.status(200).json({ success:true, data:{ product }});
+    }
     const limit = Math.min((Number(req.query.limit) || 100), 100);
     const products = await Product.find().limit(limit). populate('category').populate('owner', 'name email');
 
