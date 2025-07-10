58ce402831d03936cd2dccfa414111dcacb271d1
samiksha275 <agarwalsamiksha88@gmail.com>
2025-07-10 21:55:00 +0530

    Added the protect middleware to all the necessary routes

--- src/routes/address.router.js
+++ src/routes/address.router.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { addressHandler } = require('../controllers/address.controller');
 const {addressValidator } = require('../middlewares/address.validator');
+const { verifyJWT } = require('../middlewares/auth.middleware')
 
-router.post('/ChangeAddress', addressValidator, addressHandler);
+router.post('/ChangeAddress', verifyJWT, addressValidator, addressHandler);
 
 module.exports = router;
