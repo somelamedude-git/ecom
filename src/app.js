d7b9d56112ab23eac0f47067a60d434e3b8c1015
samiksha275 <agarwalsamiksha88@gmail.com>
2025-07-11 01:10:06 +0530

    Security middlewares added

--- src/app.js
+++ src/app.js
@@ -6,6 +6,11 @@ const rateLimit = require('express-rate-limit');
 const userRoutes = require('./routes/auth.routes');
 const addressRoutes = require('./routes/address.router');
 const productDisplayRouters = require('./routes/productDisplay.router');
+const helmet = require('helmet')
+const compression = require('compression')
+const hpp = require('hpp')
+const csrf = require('csurf')
+
 const app = express();
 
 app.use(cors({ //Yet to render the frontend, so origin is denoted through a placeholder for once
@@ -19,11 +24,15 @@ app.use(express.json({
 
 }));
 
-app.use('/user', userRoutes);
+app.use(helmet({
+    referrerPolicy: {policy: 'no-referrer'},
+}))
 
-// app.use('/edit', addressRoutes);
-
-// app.use('/api/products', productDisplayRouters);
+app.use(helmet.hidePoweredBy())
+app.use(helmet.frameguard({ action: 'deny' }))
+app.use(helmet.noSniff())
+app.use(hpp())
+app.use(compression())
 
 app.use(rateLimit({
     windowMs: 1*60*1000,
@@ -31,11 +40,20 @@ app.use(rateLimit({
     message: "Too many requests, stop or get flagged"
 }));
 
+app.use('/user', userRoutes);
+
+// app.use('/edit', addressRoutes);
+
+// app.use('/api/products', productDisplayRouters);
+
 app.use(express.urlencoded({extended:true, limit:"16kb"}));
 
 app.use(express.static("public"));
 
 app.use(cookieParser())
+
+const csrfprotection = csrf({cookie: true})
+app.use(csrfprotection)
 
 
 module.exports = { app };
