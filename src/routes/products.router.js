d997fe4fb79f378bc8e8c08b003c589837dbbad5
somelamedude-git <cozydeveloper843@gmail.com>
2025-07-13 23:59:17 +0530

    route added for cart

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { verifyJWT } = require('../middlewares/auth.middleware');
 const { addProduct, updateProductDetails } = require('../controllers/product.controller');
+const { addToBag } = require('../APIs/addToCart');
 
 router.post('/addProduct', verifyJWT, addProduct);
 router.post('/updateProduct', verifyJWT, updateProductDetails);
-
+router.post('/addToCart/:product_id', verifyJWT, addToBag);
 module.exports = router;
