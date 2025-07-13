ea0f88c4e49cd79484e2b86306e3aa653cbce833
somelamedude-git <cozydeveloper843@gmail.com>
2025-07-13 23:48:43 +0530

    bug fixed in cart api

--- src/APIs/addToCart.js
+++ src/APIs/addToCart.js
@@ -3,44 +3,46 @@ const { asyncHandler } = require('../utils/asyncHandler');
 const { ApiError } = require('../utils/ApiError');
 const { Product } = require('../models/product.models');
 
-const addToBag = asyncHandler(async(req, res)=>{ // user kind must be buyer, we will make sure of this in the middleware
+const addToBag = asyncHandler(async (req, res) => {
     const user_id = req.user._id;
     const user_ = await Buyer.findById(user_id);
 
-    if(!user_){
+    if (!user_) {
         throw new ApiError(404, 'User not found');
     }
 
     const { product_id } = req.params;
-    const product = await Product.findById(product_id).select('-stock'); //we will subtrct one from the stock when the user clicks on deliver items
+    const product = await Product.findById(product_id);
 
-    if(!product){
+    if (!product) {
         throw new ApiError(404, 'Product not found');
     }
 
-    if(product.stock == 0){
+    if (product.stock === 0) {
         throw new ApiError(409, 'Product out of stock');
     }
 
-    const existingItem = user_.cart.find(item=>item.product.toString() == product._id.toString());
+    let count_bought;
+    const existingItem = user_.cart.find(item => item.product.toString() === product._id.toString());
 
-    if(existingItem){
-        existingItem.quantity += 1; //test comment
-    }
-
-    else{
+    if (existingItem) {
+        existingItem.quantity += 1;
+        count_bought = existingItem.quantity;
+    } else {
         user_.cart.push({ product: product._id, quantity: 1 });
+        count_bought = 1;
     }
+
     await user_.save();
 
     res.status(200).json({
-        success:true,
+        success: true,
         product_id: product._id,
-        message:"Product added to cart",
-        count_bought: existingItem.quantity || 1
+        message: "Product added to cart",
+        count_bought
     });
-}) //Use the same function for wishlist
+});
 
 module.exports = {
     addToBag
-}
+};
