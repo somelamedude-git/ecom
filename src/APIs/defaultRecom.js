8cefa616c6cd07309351044e5fbe6be81ad46ef4
somelamedude-git <cozydeveloper843@gmail.com>
2025-07-15 01:35:11 +0530

    YOLO, logic done

--- src/APIs/defaultRecom.js
+++ src/APIs/defaultRecom.js
@@ -1,7 +1,19 @@
 const { Order } = require('../models/order.models');
 
+
+function countSetBits(n)
+{
+    let count = 0;
+    while (n > 0)
+    {
+        n &= (n - 1);
+        count++;
+    }
+    return count;
+}
+
 async function initializeRecommendMask(user) {
-  const peers = await Buyer.find({ ageBucket: user.ageBucket }).limit(10); //This size decreases as the database gets heavier, will figure out a function for this
+  const peers = await Buyer.find({ ageBucket: user.ageBucket }).limit(10);
   let recommend_mask = 0n;
 
   for (const peer of peers) {
@@ -10,19 +22,13 @@ async function initializeRecommendMask(user) {
   }
 
   if(recommend_mask === 0n){
-    // Here we send the most popular products, not by age group but overall, so yaha pe 1n can be there, have to give it some value for well, default
     recommend_mask = 1n;
   }
 
   user.recommend_masking = recommend_mask.toString();
 }
 
-//The logic to this will be explained in the documentation
-
-async function updateMask(user){ //This happens everytime a user adds an order, kinda like sliding window, peer to peer i can create
-    // Most likely, for users who just began, unka 1n hoga bitmask, so now we find common shit in ordersm and delete some tags
-    //We don't want faaltu ka overhead and tags, orderhistory is also needed, we flip the bits in the last one and then proceed with the next one
-    //Depending on the window size
+async function updateMask(user){
         let current_order = user.orderHistory[user.orderHistory.length-1];
         const orders = await Order.find({ _id: { $in: current_order } }).populate('product', 'bitmask');
         let mask = 0n;
@@ -30,20 +36,41 @@ async function updateMask(user){ //This happens everytime a user adds an order, 
         for(const order of orders){
             mask = mask|BigInt(order.product.bitmask || '0');
         }
-
-    if(user.orderHistory.length === 1){ // We turn te mask to 0 again, we need or conditions not and, optimal
-        // Now we dive into well, the array of arrays
+    if(user.orderHistory.length === 1){ 
         user.prev_order_bit=mask.toString();
         user.recommend_masking = mask.toString();
     }
-
     else if(user.orderHistory.length>=2){
-        const final_mask = BigInt(user.prev_order_bit || '0') & mask; // A common ground between past 2 orders
+        const final_mask = BigInt(user.prev_order_bit || '0') & mask; 
         user.prev_order_bit = mask.toString();
         user.recommend_masking = final_mask.toString();
-
     }
+
+  const same_age_buyers = await Buyer.find(
+  { ageBucket: user.ageBucket },
+  { recommend_masking: 1 }
+);
+  const buyers_bitmask = same_age_buyers_bitmask.map(mask => {
+  const bitmask = BigInt(mask);
+  const ratio = countSetBits(bitmask ^ BigInt(user.recommend_mask)) / countSetBits(bitmask | BigInt(user.recommend_mask));
+  return [ratio, mask]; 
+});
+
+  buyers_bitmask.sort((a, b) => a[0] - b[0]);
+
+  const quartile_one = 0.25*buyers_bitmask.length; const quartile_three = 0.75*buyers_bitmask.length;
+  // Now between quartile one and three is where the golden ratio lies, thats where i pick my data from, this where randomness is introduced, but not too much
+
+  let new_mask = 1n;
+  for(let i = quartile_one; i<quartile_three; i++){
+    new_mask = new_mask & BigInt(buyers_bitmask[i][1]);
+  }
+
+  user.recommend_masking = (BigInt(user.recommend_masking) | new_mask).toString();
+
 }
+
+
 
 module.exports = {
     initializeRecommendMask,
