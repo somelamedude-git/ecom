61435b1a1310ad0ba85d69d9634f63cf88549c9e
somelamedude-git <cozydeveloper843@gmail.com>
2025-07-21 22:05:53 +0530

    kill me

--- src/APIs/addToCart.js
+++ src/APIs/addToCart.js
@@ -6,39 +6,33 @@ const { Product } = require('../models/product.models');
 const addToBag = asyncHandler(async (req, res) => {
     const user_id = req.user._id;
     const user_ = await Buyer.findById(user_id);
+    if (!user_) throw new ApiError(404, 'User not found');
 
-    if (!user_) {
-        throw new ApiError(404, 'User not found');
-    }
-
-    const { product_id } = req.params; // Te product variants have color and size
+    const { product_id } = req.params;
     const { size_ } = req.body;
+
+    if (!size_) throw new ApiError(400, 'Size is required');
+
     const product = await Product.findById(product_id);
-    
-    if (!product) {
-        throw new ApiError(404, 'Product not found');
-    }
-     const product_info = product.variants.find(variant=>
-        variant.size === size_
-    );
+    if (!product) throw new ApiError(404, 'Product not found');
 
-    const stock_ = product_info.stock;
+    const product_info = product.variants.find(v => v.size === size_);
+    if (!product_info) throw new ApiError(400, 'Invalid size');
 
-    if (stock_ === 0) {
+    if (product_info.stock === 0) {
         throw new ApiError(409, 'Product out of stock');
     }
 
-    let count_bought;
-    const existingItem = user_.cart.find(item => (item.product.toString() === product._id.toString()) && (item.size===size_) );
+    const alreadyInCart = user_.cart.find(item =>
+        item.product.toString() === product_id &&
+        item.size === size_
+    );
 
-    if (existingItem) {
-        existingItem.quantity += 1;
-        count_bought = existingItem.quantity;
-    } else {
-        user_.cart.push({ product: product._id, quantity: 1, size:size_ });
-        count_bought = 1;
+    if (alreadyInCart) {
+        throw new ApiError(409, 'Product with this size already in cart');
     }
 
+    user_.cart.push({ product: product._id, quantity: 1, size: size_ });
     await user_.save();
 
     res.status(200).json({
@@ -46,9 +40,10 @@ const addToBag = asyncHandler(async (req, res) => {
         product_id: product._id,
         message: "Product added to cart",
         size: size_,
-        count_bought
+        count_bought: 1
     });
 });
+
 
 module.exports = {
     addToBag
