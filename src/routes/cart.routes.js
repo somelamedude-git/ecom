3185678e8bac0cc1d7d8ba3f73d19a07fe391858
somelamedude-git <cozydeveloper843@gmail.com>
2025-07-22 18:33:52 +0530

    increment router added

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const { verifyJWT } = require('../middlewares/auth.middleware');
 const { fetchCart } = require('../APIs/fetchCart');
 const { filterOutItems } = require('../middlewares/fetchCart.middleware');
+const { incrementItem, decrementItem } = require('../APIs/addToCart');
+const { changeQuantUtil } = require('../utils/cartUtility');
 
 router.get('/view', verifyJWT, filterOutItems, fetchCart);
+router.patch('/increment/:product_id', verifyJWT, changeQuantUtil, incrementItem);
 
 module.exports = router;
