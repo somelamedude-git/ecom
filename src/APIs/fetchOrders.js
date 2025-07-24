f1a3cdf8bbef697462a5c2a26dea0a475adb299f
somelamedude-git <cozydeveloper843@gmail.com>
2025-07-25 00:08:58 +0530

    previous getOrders restored

--- src/APIs/fetchOrders.js
+++ src/APIs/fetchOrders.js
@@ -1,51 +1,43 @@
 const { Order } = require("../models/order.models");
-const { Buyer } = require("../models/user.models");
+const { BaseUser } = require("../models/user.models");
 const { ApiError } = require("../utils/ApiError");
 const { asyncHandler } = require("../utils/asyncHandler");
 
-const getOrders = asyncHandler(async(req, res)=>{
-    const user_id = req.user._id;
-    const user = await Buyer.findById(user_id).select();
+const getOrders = asyncHandler(async (req, res) => {
+    const user = req.user._id
+    const {status} = req.query
 
-    if(!user) throw new ApiError(404, 'User not found');
+    const customer = await BaseUser.findById(user)
+
+    if(!customer)
+        throw new ApiError(404, "User not found")
+
+    let query = {
+        customer: customer._id
+    }
+
+    const allowedStatus = [
+        'pending',
+        'delivered',
+        'cancelled',
+        'schedule_return',
+        'returned',
+        'approve_return',
+        'shipped'
+    ]
+
+    if(status && allowedStatus.includes(status))
+        query.status = status
+
+    const orders = await Order.find(query).populate('product').sort({createdAt: -1})
+
+    return res.status(200).json({
+        status: true,
+        count: orders.length,
+        orders
+    })
 
 })
-
-// const getOrders = asyncHandler(async (req, res) => {
-//     const user = req.user._id
-//     const {status} = req.query
-
-//     const customer = await BaseUser.findById(user)
-
-//     if(!customer)
-//         throw new ApiError(404, "User not found")
-
-//     let query = {
-//         customer: customer._id
-//     }
-
-//     const allowedStatus = [
-//         'pending',
-//         'delivered',
-//         'cancelled',
-//         'schedule_return',
-//         'returned',
-//         'approve_return',
-//         'shipped'
-//     ]
-
-//     if(status && allowedStatus.includes(status))
-//         query.status = status
-
-//     const orders = await Order.find(query).populate('product').sort({createdAt: -1})
-
-//     return res.status(200).json({
-//         status: true,
-//         count: orders.length,
-//         orders
-//     })
-
-// })
 
 module.exports= {
     getOrders
