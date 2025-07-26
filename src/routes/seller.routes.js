2dd6b29fdb98606fb2bab23a5939b5ba571c8947
somelamedude-git <cozydeveloper843@gmail.com>
2025-07-26 22:58:13 +0530

    mirinda

--- src/routes/seller.routes.js
+++ src/routes/seller.routes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { fetchSalesMap } = require('../APIs/heatmap.api');
 const { verifyJWT } = require('../middlewares/auth.middleware');
+const { fetchSellerProducts } = require('../APIs/products.api');
 
 router.get('/SalesMap', verifyJWT, fetchSalesMap);
+router.get('/productList', verifyJWT, fetchSellerProducts);
 
 module.exports = router;
