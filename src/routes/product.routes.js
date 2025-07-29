ac6a72bcb4426680a0d0782fc9897eab3aac47d8
somelamedude-git <cozydeveloper843@gmail.com>
2025-07-29 11:58:56 +0530

    cleaned up

--- src/routes/product.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { productAnalysis } = require('../APIs/products.api');
-const {verifyJWT} = require('../middlewares/auth.middleware');
-
-router.get('/:product_id/analytics', verifyJWT, productAnalysis);
-
-module.exports = router;
--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { verifyJWT } = require('../middlewares/auth.middleware');
 const { addProduct, updateProductDetails } = require('../controllers/product.controller');
+const { productAnalysis } = require('../APIs/products.api');
 const { addToBag } = require('../APIs/addToCart');
 
 router.post('/addProduct', verifyJWT, addProduct);
 router.post('/updateProduct', verifyJWT, updateProductDetails);
 router.post('/addToCart/:product_id', verifyJWT, addToBag);
+router.get('/:product_id/analytics', verifyJWT, productAnalysis);
+
 module.exports = router;
