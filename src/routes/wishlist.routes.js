7a8f2855740bd22c0610d9d49bc464dde3f67a74
somelamedude-git <cozydeveloper843@gmail.com>
2025-07-31 22:00:09 +0530

    error fized

--- src/routes/wishlist.routes.js
+++ src/routes/wishlist.routes.js
@@ -1,7 +1,7 @@
 const { addToWishList, removeFromWL, fetchWishList } = require('../APIs/wishlistAPI');
 const express = require('express');
 const { verifyJWT } = require('../middlewares/auth.middleware');
-const router = express.Router;
+const router = express.Router();
 
 router.get('/getItems', verifyJWT, fetchWishList);
 router.delete('/deleteItem/:product_id', verifyJWT, removeFromWL);
