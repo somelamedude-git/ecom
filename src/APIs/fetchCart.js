6f592d371a5d0e98086fae59e89780b69fd6f92a
somelamedude-git <cozydeveloper843@gmail.com>
2025-08-02 16:44:46 +0530

    tired

--- src/APIs/fetchCart.js
+++ src/APIs/fetchCart.js
@@ -2,8 +2,11 @@ const { asyncHandler } = require('../utils/asyncHandler');
 const { ApiError } = require('../utils/ApiError');
 
 const fetchCart = asyncHandler(async(req, res)=>{
+    console.log('fetching the cart api');
+    console.log(req.cart);
     const cart_items = req.cart;
     const cart_length = cart_items.length;
+    console.log(cart_length);
 
     return res.status(200).json({ // let's just put in stock or not only in fetch cart, bingo
         success: true,
