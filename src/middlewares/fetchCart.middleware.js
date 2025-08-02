53d4642ba44fcacab4383b59b1ed8c5af14ee710
somelamedude-git <cozydeveloper843@gmail.com>
2025-08-02 16:19:13 +0530

    ill die

--- src/middlewares/fetchCart.middleware.js
+++ src/middlewares/fetchCart.middleware.js
@@ -3,18 +3,20 @@ const { ApiError } = require('../utils/ApiError');
 const { Buyer } = require('../models/user.models');
 
 const filterOutItems = asyncHandler(async (req, res, next) => {
+    console.log('Entered the middleware of filtering items out')
     const user_id = req.user._id;
-    const user = await Buyer.findById(user_id).populate("cart.product");
+    const user = await Buyer.findById(user_id.toString()).populate("cart.product");
 
     if (!user) {
+        console.log('In cart middleware, user not found');
         throw new ApiError(404, 'User not found');
     }
 
+    console.log('User found, I am in cart middleware');
     user.cart = (user.cart || []).filter(item => item.quantity > 0);
     await user.save();
 
     req.cart = user.cart; // full data jayega ismein 
-    
     next(); 
 });
 
