6eea14e8f394f80e143d0485f8fb4a854fb85c33
somelamedude-git <cozydeveloper843@gmail.com>
2025-08-04 00:35:04 +0530

    predefined categories added

--- categoryAdder.js
+++ categoryAdder.js
@@ -1,2 +1,55 @@
+const { Category } = require('./src/models/category.models');
 const mongoose = require('mongoose');
-const 
+const { DB_NAME } = require('./src/constants');
+require('dotenv').config({ path: './.env' });
+
+const connectDB = async()=>{
+    try{
+        const conn = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`, {
+        });
+        console.log(conn);
+    } catch(error){
+        console.error(error);
+        throw error;
+    }
+}
+
+const addCategories = async (categories = []) => {
+  try {
+    await connectDB();
+    await Category.insertMany(categories);
+    console.log('Categories added successfully!');
+  } catch (err) {
+    console.error('Error adding categories:', err);
+    throw new Error('Failed to insert categories');
+  }finally{
+    await mongoose.disconnect();
+  }
+};
+
+const categories = [{
+    name: "Men's",
+    products: []
+},
+{
+    name: "Women's",
+    products: []
+},
+
+{
+    name: "Footwear",
+    products: []
+},
+
+{
+    name: "Accessories",
+    products: []
+},
+
+{
+    name: "Makeup",
+    products: []
+}
+]
+
+addCategories(categories);
