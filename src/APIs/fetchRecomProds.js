e90dcfe04262e4517b6bd3a6606b09bcbd7ee32b
somelamedude-git <cozydeveloper843@gmail.com>
2025-08-04 18:05:06 +0530

    new shit

--- src/APIs/fetchRecomProds.js
+++ src/APIs/fetchRecomProds.js
@@ -23,10 +23,21 @@ const fetchRecomProds = asyncHandler(async(req, res)=>{ // idiot, this is just f
         const bitmask_of_product = BigInt(product.bitmask);
         const union = bitmask_of_product | user_choice_bitmask;
         const intersection = ~(bitmask_of_product ^ user_choice_bitmask);
+        const ratio = union === 0n ? 1 : Number(countSetBits(intersection)) / Number(countSetBits(union));
 
+        return [ratio, product]
+    });
+
+    informative_products.sort((a,b)=> a[0]-b[0]);
+    const q1 = Math.floor(0.25 * informative_products.length);
+    const q3 = Math.floor(0.75 * informative_products.length);
+
+    let recommended_products = informative_products.slice(q1, q3); // Just don't extract the ratio from this in the frontend, or do
+
+    res.status(200).json({
+        success: true,
+        recommended_products
     })
-
-
 });
 
 module.exports = {
