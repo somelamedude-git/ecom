9abba4f926de4ae619d67a506e42f2f55e735be3
somelamedude-git <cozydeveloper843@gmail.com>
2025-08-05 16:15:01 +0530

    a very suitable api

--- src/APIs/checkLoginStatus.js
+++ src/APIs/checkLoginStatus.js
@@ -1,12 +1,13 @@
 const { asyncHandler } = require('../utils/asyncHandler');
-const { ApiError } = require('../utils/ApiError');
 const { getUserFromToken } = require('../middlewares/auth.middleware.js');
 
 const isLoggedIn = asyncHandler(async(req,res)=>{
     console.log('entered isLoggedIn');
     const loggedIn = !!req.user;
+    const userKind = req.user.kind || null
          return res.status(200).json({
-         isLoggedIn: loggedIn
+         isLoggedIn: loggedIn,
+         userType: userKind
          });
 })
 
