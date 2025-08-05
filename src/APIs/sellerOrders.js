aee62c426bff6af8c6e6cca7699d711feb14afb7
somelamedude-git <cozydeveloper843@gmail.com>
2025-08-06 03:42:43 +0530

    status update api added

--- src/APIs/sellerOrders.js
+++ src/APIs/sellerOrders.js
@@ -5,7 +5,7 @@ const { Seller } = require('../models/user.models');
 const fetchSellerOrders = asyncHandler(async (req, res) => {
     const user_id = req.user._id;
     
-    const user = await Seller.findById(user_id.toString()).populate("order_quo");
+    const user = await Seller.findById(user_id.toString()).select("order_quo").populate("order_quo").lean();
     if (!user) throw new ApiError(404, 'User not found');
 
     let { page = 1, limit = 10, filter = "" } = req.query;
@@ -29,6 +29,28 @@ const fetchSellerOrders = asyncHandler(async (req, res) => {
     });
 });
 
+const updateStatus = asyncHandler(async (req, res) => {
+    const { orderId } = req.params;
+    const user_id = req.user._id;
+    const { status } = req.body;
+
+    const user = await Seller.findById(user_id).select("order_quo").populate("order_quo");
+    if (!user) throw new ApiError(404, 'User not found');
+
+    const order = user.order_quo.find(order => order._id.toString().equals(orderId.toString()));
+    if (!order) throw new ApiError(400, 'You are not authorized to change this order');
+
+    order.status = status;
+    await order.save();
+
+    res.status(200).json({
+        success: true,
+        message: "Status updated"
+    });
+});
+
+
 module.exports = {
-    fetchSellerOrders
+    fetchSellerOrders,
+    updateStatus
 }
--- src/routes/seller.routes.js
+++ src/routes/seller.routes.js
@@ -5,10 +5,12 @@ const { verifyJWT } = require('../middlewares/auth.middleware');
 const { fetchSellerProducts } = require('../APIs/products.api');
 const { productAnalysis } = require('../APIs/products.api');
 const { sellerStats } = require('../APIs/sellerStats');
+const { fetchSellerOrders } = require('../APIs/sellerOrders');
 
 router.get('/SalesMap', verifyJWT, fetchSalesMap);
 router.get('/productList', verifyJWT, fetchSellerProducts);
 router.get('/:product_id/analytics', verifyJWT, productAnalysis);
 router.get('/stats', verifyJWT, sellerStats);
+router.get('/orders', verifyJWT, fetchSellerOrders);
 
 module.exports = router;
