d193fc38dde429b189beedc8a65a46b7f4cce154
somelamedude-git <cozydeveloper843@gmail.com>
2025-08-06 03:49:31 +0530

    hcf

--- src/routes/seller.routes.js
+++ src/routes/seller.routes.js
@@ -6,11 +6,13 @@ const { fetchSellerProducts } = require('../APIs/products.api');
 const { productAnalysis } = require('../APIs/products.api');
 const { sellerStats } = require('../APIs/sellerStats');
 const { fetchSellerOrders } = require('../APIs/sellerOrders');
+const { updateStatus } = require('../APIs/sellerOrders');
 
 router.get('/SalesMap', verifyJWT, fetchSalesMap);
 router.get('/productList', verifyJWT, fetchSellerProducts);
 router.get('/:product_id/analytics', verifyJWT, productAnalysis);
 router.get('/stats', verifyJWT, sellerStats);
 router.get('/orders', verifyJWT, fetchSellerOrders);
+router.patch('/orders/:orderId/status', verifyJWT, updateStatus);
 
 module.exports = router;
