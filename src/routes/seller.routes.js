e295e07b70d2e6005fa2ad40626277a318f8ce46
somelamedude-git <cozydeveloper843@gmail.com>
2025-08-05 19:32:26 +0530

    stats routing added

--- src/routes/seller.routes.js
+++ src/routes/seller.routes.js
@@ -4,9 +4,11 @@ const { fetchSalesMap } = require('../APIs/heatmap.api');
 const { verifyJWT } = require('../middlewares/auth.middleware');
 const { fetchSellerProducts } = require('../APIs/products.api');
 const { productAnalysis } = require('../APIs/products.api');
+const { sellerStats } = require('../APIs/sellerStats');
 
 router.get('/SalesMap', verifyJWT, fetchSalesMap);
 router.get('/productList', verifyJWT, fetchSellerProducts);
 router.get('/:product_id/analytics', verifyJWT, productAnalysis);
+router.get('/stats', verifyJWT, sellerStats);
 
 module.exports = router;
