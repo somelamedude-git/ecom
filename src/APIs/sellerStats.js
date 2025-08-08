6453a7100d2adc295e0f03faac61674b46a5c652
somelamedude-git <cozydeveloper843@gmail.com>
2025-08-08 14:03:24 +0530

    seller profile api added

--- src/APIs/sellerStats.js
+++ src/APIs/sellerStats.js
@@ -22,6 +22,25 @@ const sellerStats = asyncHandler(async(req, res)=>{
     });
 });
 
+const sellerProfile = asyncHandler(async(req, res)=>{
+    const user_id = req.user._id;
+    const user = await Seller.findById(user_id.toString()).select("-password")
+
+    if(!user) throw new ApiError(404, 'User not found');
+    const number_of_products = user.selling_products.length; // To be displayed in the prfile but add nav to it
+    const number_of_orders = user.order_quo.length; // Same with this one
+
+    // creat a nav for store of the user in the frontend too, not sending with this api
+
+    res.status(200).json({
+        success: true,
+        user,
+        number_of_products,
+        number_of_orders
+    });
+})
+
 module.exports = {
-    sellerStats
+    sellerStats,
+    sellerProfile
 }
