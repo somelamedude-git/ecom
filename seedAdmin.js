f9874e679fe971aa9d7bd28f1b16325921532e52
samiksha275 <agarwalsamiksha88@gmail.com>
2025-08-12 03:08:30 +0530

    Mails added for admin

--- seedAdmin.js
+++ seedAdmin.js
@@ -1,26 +1,27 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import { connectDB } from './src/db/db.js';
-import { Admin } from './src/models/user.models.js';
+import { connectDB } from './backend/src/db/db.js';
+import { Admin } from './backend/src/models/user.models.js';
 
 try {
   await connectDB();
   console.log("MongoDB connected");
 
-  const admin = await Admin.find({})
-//   const admin = new Admin ({
-//     username: 'admin1',
-//     email: '',
-//     password: 'admin1235',
-//     name: 'Admin User',
-//     phone_number: '1234567890',
-//     age: 25,
-//     isVerified: true,
-//     user_management: ['ban_user', 'unban_user', 'delete_user'],
-//     product_management: ['edit_product', 'delete_product'],
-//     review_management: ['delete_review']
-//   })
+  const admin = new Admin ({
+    username: '',
+    email: '',
+    password: '',
+    name: '',
+    phone_number: '',
+    age: 25,
+    isVerified: true,
+    user_management: ['ban_user', 'unban_user', 'delete_user'],
+    product_management: ['edit_product', 'delete_product'],
+    review_management: ['delete_review']
+  })
+
+  await admin.save()
   console.log(admin)
   console.log("Admin user created successfully");
 
