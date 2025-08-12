d23b4210b8ef6803585fcd1ea77465f34bee7f9f
somelamedude-git <cozydeveloper843@gmail.com>
2025-08-12 17:14:10 +0530

    code addition api added

--- backend/src/APIs/promo.api.js
+++ backend/src/APIs/promo.api.js
@@ -1,12 +1,12 @@
 const { Promo } = require('../models/promo.model');
 const { asyncHandler } = require('../utils/asyncHandler');
 const { ApiError } = require('../utils/ApiError');
-const { Buyer } = require('../models/user.models');
+const { Buyer, Admin } = require('../models/user.models');
 
 const applyPromo = asyncHandler(async(req, res)=>{
     const user_id = req.user._id;
-    const { total_cost, code_used } = req.body;
-
+    let { total_cost, code_used } = req.body;
+    total_cost = Number(total_cost);
     const user = await Buyer.findById(user_id.toString());
     if(!user) throw new ApiError(404, 'User not found');
 
@@ -29,6 +29,28 @@ const applyPromo = asyncHandler(async(req, res)=>{
     })
 });
 
+const addCode = asyncHandler(async(req, res)=>{
+    const user_id = req.user._id;
+    const admin = Admin.findById(user_id.toString());
+    if(!admin) throw new ApiError(400, 'Unauthorized access');
+
+    let { code, discount } = req.body;
+    discount = Number(discount);
+    const new_code = await Promo.create({
+        code: code,
+        discount_provided:discount,
+        used_by: []
+    });
+
+    await new_code.save();
+
+    res.status(200).json({
+        success: true,
+        message: 'Promo code added successfully'
+    })
+})
+
 module.exports = {
-    applyPromo
+    applyPromo,
+    addCode
 }
